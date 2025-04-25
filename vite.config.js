c4a04068c026841a66a1a996f76b862366c7b6d2
Eastleigh <dxinone@gmail.com>
2025-04-24 23:54:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-  build: {
-    rollupOptions: {
-      input: './app/entry.client.jsx',
-    },
-  },
+  plugins: [react()],
+  root: './', // Ensures Vite uses root directory
+  publicDir: 'public', // Serves files from `public`
 });
